4f75be070b6c9d0ad4213fb4f6b3ead6b000acaf
unknown <seaspatton13@gmail.com>
2019-09-24 15:55:13 -0400

    timer working but source files not linking correctly

---
+++ assets/javascript/app.js
@@ -0,0 +1,56 @@
+// timer
+// var x = setInterval(function() {
+
+// var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+// document.getElementById("timer").innerHTML = seconds + "s ";
+
+// if (distance < 0) {
+//     clearInterval(x);
+//     document.getElementById("timer").innerHTML = "EXPIRED";
+//   }
+// },1000);
+
+// document.getElementById('timer').innerHTML =
+//   03 + ":" + 00;
+// startTimer();
+
+// function startTimer() {
+//   var presentTime = document.getElementById('timer').innerHTML;
+//   var timeArray = presentTime.split(/[:]+/);
+//   var m = timeArray[0];
+//   var s = checkSecond((timeArray[1] - 1));
+//   if(s==59){m=m-1}
+//   //if(m<0){alert('timer completed')}
+  
+//   document.getElementById('timer').innerHTML =
+//     m + ":" + s;
+//   setTimeout(startTimer, 1000);
+// }
+
+// function checkSecond(sec) {
+//   if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
+//   if (sec < 0) {sec = "59"};
+//   return sec;
+// }
+
+var timeleft = 60;
+var downloadTimer = setInterval(function(){
+  document.getElementById("timer").innerHTML = timeleft + " seconds remaining";
+  timeleft -= 1;
+  if(timeleft <= 0){
+    clearInterval(downloadTimer);
+    document.getElementById("timer").innerHTML = "Finished"
+  }
+}, 1000);
+
+// declare variables
+var correctCount = 0;
+var wrongCount = 0;  
+var unanswered = 0;
+
+// hide the questions until the game starts 
+
+$('#countdown').hide();
+$('.trivia-ques').hide();
+$('.results').hide();
